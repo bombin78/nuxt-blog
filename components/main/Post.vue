87b5d84e8d2695bf0560724123f5f2b60ff27c4e
bombin78 <bombin78@mail.ru>
2020-03-22 00:53:44 +0300

    component "Post"

--- components/main/Post.vue
+++ components/main/Post.vue
@@ -1,18 +1,60 @@
 <template>
-  <article class="post">
+  <el-card
+    shadow="hover"
+    :body-style="{padding: 0}"
+    class="post">
+    <header slot="header" class="post-header">
+      <h3>Post title</h3>
+      <small>
+        <i class="el-icon-time"></i>
+        {{ new Date().toLocaleString()}}</small>
+    </header>
+    <div class="post-body">
+      <img
+        src="https://wallbox.ru/resize/800x480/wallpapers/main2/201726/14986676245953da68df7d32.08263646.jpg"
+        alt="Морская крепость Свеаборг"
+        class="post-img">
+    </div>
+    <footer class="post-footer">
+      <el-button round @click="openPost">Открыть</el-button>
 
-  </article>
+      <span>
+        <i class="el-icon-message"></i>
+        12
+      </span>
+    </footer>
+  </el-card>
 </template>
 
 <script>
 export default {
+  methods: {
+    openPost() {}
+  },
 }
 </script>
 
 <style lang="scss" scoped>
+
   .post {
-    height: 220px;
-    border: 1px solid black;
-    margin-bottom: 2rem;
+    margin-bottom: 1.5rem;
+  }
+
+  .post-header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  .post-img {
+    width: 100%;
+    height: auto;
+  }
+
+  .post-footer {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 1rem;
   }
 </style>
