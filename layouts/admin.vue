42ebf6c3d439fece6df764bf6003971c22a67f85
bombin78 <bombin78@mail.ru>
2020-04-12 23:01:58 +0300

    Панель администратора: структура страниц

---
+++ layouts/admin.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="wrap">
+    <el-container :style="{height: '100%'}">
+      <el-aside width="200px">
+        Aside
+      </el-aside>
+      <el-main>
+        <nuxt />
+      </el-main>
+    </el-container>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="scss" scoped>
+  .wrap {
+    width: 100%;
+    height: 100vh;
+  }
+</style>
---
+++ pages/admin/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <h1>Welcome to admin page</h1>
+</template>
+
+<script>
+export default {
+  layout: 'admin',
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
